2d49b4540da6fae6ba0459d879f65a3c4bc1ba66
Tanzeel Rehman <tanzeel.r@turing.com>
2024-08-26 01:57:32 +0500

    add 3x routes for /messages

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', { encoding: 'utf8' });
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    return JSON.parse(contents);
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    const id = Math.round(Math.random() * 999);
+    messages[id] = { id, content };
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
